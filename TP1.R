78c6bb29fdce6f403fdc3fd135f889a9cdc4a431
ams <ams.gh@hmamail.com>
2016-11-29 14:53:31 +0100

    TP1 : montyhall game ok
    filled code for montyhall fct, full game OK

--- TP1.R
+++ TP1.R
@@ -1,15 +1,50 @@
-#!/usr/bin/env Rscript
+#!/usr/bin/Rscript
+
+readinteger <- function(){ 
+  n <- readline("Enter an integer: ") #ohoh -> interactive mode ONLY = not working in shell
+  return(as.integer(n))
+}
 
 montyhall <- function(){
-  goat <- 'Goat'
-  car <- 'Car'
+  ### setup ###
+  i <- 1 #misc loop counter (OMG arrays start at 1)
+  str.goat <- 'Goat'
+  str.car <- 'Car'
   
-  list <- c(car, goat, goat) #compose
-  list <- sample(list, length(list)) #randomize
+  doors <- c(str.car, str.goat, str.goat) #compose
+  doors <- sample(doors) #randomize
   
-  #print(list)
+  ### player : select door ###
+  player.choice <- sample(1:length(doors) , size=1)
+  cat("[Player] Choice =", player.choice, '\n')
   
-  return(list[1]) #select first item of randomized array
+  for(i in 1:length(doors)){
+    if((i!=player.choice) || (doors[i]!=str.car)){
+      #search door to open : not the player's choice & not the car
+      presenter.choice <- i
+      cat("[Presenter] Choice =", presenter.choice, "(", doors[presenter.choice], ')\n')
+      break()
+    }
+  }
+  
+  ### player : keep same door ? ###
+  keep <- sample(c(TRUE,FALSE), size=1)
+  cat('[Player] Keep door', player.choice, '?', keep, '\n')
+  
+  if(keep==FALSE){
+    for(i in 1:length(doors)){
+      if((i!=player.choice) || (i!=presenter.choice)){
+        #search last door
+        player.choice <- i
+      }
+    }
+  }
+
+  ### final : open door ###
+  cat("[Player] Final choice =", player.choice, "(", doors[player.choice], ')\n')
+  cat("[Presenter] Doors were :", doors, '\n')
+  
+  return('Exit succes') #select first item of randomized array
 }
 
 montyhall()
