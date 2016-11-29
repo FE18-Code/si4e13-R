ffbf8850a47c537eafd309154f1f75a528fe149c
ams <ams.gh@hmamail.com>
2016-11-29 15:39:02 +0100

    simulation + conditions fix
    added simulation fct
    fixed bad conditions :)
    improved output formatting

--- TP1.R
+++ TP1.R
@@ -1,11 +1,16 @@
 #!/usr/bin/Rscript
 
-readinteger <- function(){ 
+
+# read from stdin
+readinteger <- function(){ #unused
   n <- readline("Enter an integer: ") #ohoh -> interactive mode ONLY = not working in shell
   return(as.integer(n))
 }
 
+
+# main game fct
 montyhall <- function(){
+  cat('\n===== New game =====\n')
   ### setup ###
   retval <- -1
   i <- 1 #misc loop counter (OMG arrays start at 1)
@@ -20,7 +25,7 @@ montyhall <- function(){
   cat("[Player] Choice =", player.choice, '\n')
   
   for(i in 1:length(doors)){
-    if((i!=player.choice) || (doors[i]!=str.car)){
+    if((i!=player.choice) && (doors[i]!=str.car)){
       #search door to open : not the player's choice & not the car
       presenter.choice <- i
       cat("[Presenter] Choice =", presenter.choice, "(", doors[presenter.choice], ')\n')
@@ -30,13 +35,15 @@ montyhall <- function(){
   
   ### player : keep same door ? ###
   keep <- sample(c(TRUE,FALSE), size=1)
+  #keep <- TRUE
   cat('[Player] Keep door', player.choice, '?', keep, '\n')
   
   if(keep==FALSE){
     for(i in 1:length(doors)){
-      if((i!=player.choice) || (i!=presenter.choice)){
+      if((i!=player.choice) && (i!=presenter.choice)){
         #search last door
         player.choice <- i
+        break()
       }
     }
   }
@@ -49,4 +56,18 @@ montyhall <- function(){
   return(retval) #select first item of randomized array
 }
 
-montyhall()
+
+# simulation fct (tested nb.iter times)
+stats <- function(nb.iter){
+  results <- c() #init empty
+
+  for(i in 1:nb.iter){
+    results <- c(results, montyhall())
+  }
+
+  return(results)
+}
+
+
+table(stats(100))
+
